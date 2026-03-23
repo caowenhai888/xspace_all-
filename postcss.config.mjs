4844f2855bdb08021508be15fa17d3511678494a
caowenhai <caowenhai@bytedance.com>
2026-03-23 17:35:48 +0800

    解决safaribug

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,13 @@
-const config = {
+export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {}, // 1. 先跑 Tailwind v4 生成原始 CSS (带 @layer 和 oklch)
+    'postcss-preset-env': {     // 2. 后跑这个，负责“剥壳”和“转化”
+      stage: 2,
+      features: {
+        'cascade-layers': true, // 核心：自动删掉 @layer utilities 等外壳，兼容旧 Safari
+        'oklch-function': true  // 核心：把 oklch 颜色转为 rgb，解决颜色不显示问题
+      }
+    },
+    'autoprefixer': {},         // 3. 最后加前缀
   },
-};
-
-export default config;
+}
